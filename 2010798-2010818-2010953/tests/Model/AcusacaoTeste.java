e6faffbe614ebdad7bde0ea0f468ce9653e91976
bia <mariabeatrizsobreira@gmail.com>
2022-11-15 01:17:19 -0300

    Add AcusacaoTeste

--- 2010798-2010818-2010953/tests/Model/AcusacaoTeste.java
+++ 2010798-2010818-2010953/tests/Model/AcusacaoTeste.java
@@ -9,14 +9,38 @@ public class AcusacaoTeste {
 	Pessoa acusadorPessoa;
 	Pessoa acusadoPessoa;
 	Acusacao novaAcusacao;
+	Cartas []testCartas;
+	Cartas []test2Cartas;
+	Cartas []envelope;
 	
 	@Before
 	public void setUp() {
-
+		novaAcusacao = new Acusacao("Professor Plum", "Srta. Scarlet", "Entrada", "Corda");
+		
+		envelope = new Cartas[] 
+				{
+						new Cartas("arma", "Corda"),
+						new Cartas("suspeito", "Srta. Scarlet"),
+						new Cartas("comodo", "Entrada"),
+				};
+		testCartas = new Cartas[] 
+				{
+						new Cartas("arma", "Corda"),
+						new Cartas("suspeito", "Srta. Scarlet"),
+						new Cartas("comodo", "Entrada"),
+				};
+		test2Cartas = new Cartas[] 
+				{
+						new Cartas("arma", "Faca"),
+						new Cartas("suspeito", "Professor Plum"),
+						new Cartas("comodo", "Sala de jantar"),
+				};
 	}
 	
 	@Test
 	public void acusacaoTests() {
+		assertTrue(novaAcusacao.isAcusacaoTrue(testCartas));
+		assertFalse(novaAcusacao.isAcusacaoTrue(test2Cartas));
 		
 	}
 }
